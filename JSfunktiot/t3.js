35256681fec91b80a630fc637bdb0d501462ee57
Elina Sankila <sankilaelina@gmail.com>
2023-08-29 14:00:43 +0300

    tehtävät lisätty

--- JSfunktiot/t3.js
+++ JSfunktiot/t3.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 
 const numbers = [];
 
 let continueEntering = true;
 while (continueEntering) {
-    const input = prompt("Enter a number (or 'done' to finish): ");
+  const input = prompt("Enter a number (or 'done' to finish): ");
 
-    if (input === 'done') {
-        continueEntering = false;
-    } else {
-        const number = parseFloat(input);
-        if (!isNaN(number)) {
-            numbers.push(number);
-        }
+  if (input === "done") {
+    continueEntering = false;
+  } else {
+    const number = parseFloat(input);
+    if (!isNaN(number)) {
+      numbers.push(number);
     }
+  }
 }
 
 const evenNumbers = [];
 
 for (const num of numbers) {
-    if (num % 2 === 0) {
-        evenNumbers.push(num)
-        document.getElementById('kohde1').innerHTML = "Even Numbers:" + evenNumbers;
-    } else {
-        document.getElementById('kohde1').innerHTML = "Even Numbers: none"
-    }
+  if (num % 2 === 0) {
+    evenNumbers.push(num);
+    document.getElementById("kohde1").innerHTML = "Even Numbers:" + evenNumbers;
+  } else {
+    document.getElementById("kohde1").innerHTML = "Even Numbers: none";
+  }
 }
 
-document.getElementById('kohde2').innerHTML = "Program has ended.";
+document.getElementById("kohde2").innerHTML = "Program has ended.";
