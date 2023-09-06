c1b5a6f9e6797e671e65fb1d3ccc26b19b7d86f9
Elina <sankilaelina@gmail.com>
2023-09-06 19:03:49 +0300

    ajax t5

--- JSAjax/t5.js
+++ JSAjax/t5.js
@@ -1,38 +1,40 @@
-"use strict";
-const modal = document.querySelector("dialog");
-modal.addEventListener("click", () => {
+'use strict';
+const modal = document.querySelector('dialog');
+modal.addEventListener('click', function () {
   modal.close();
 });
 
-const apiUrl = "https://sodexo-webscrape-r73sdlmfxa-lz.a.run.app/api/v1";
+const apiUrl = 'https://sodexo-webscrape-r73sdlmfxa-lz.a.run.app/api/v1';
 const positionOptions = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0,
 };
 
-const fetchData = async (url, options = {}) => {
+async function fetchData(url, options = {}) {
   const response = await fetch(url, options);
   if (!response.ok) {
     throw new Error(`Error ${response.status} occured`);
   }
   const json = response.json();
   return json;
-};
+}
 
-const calculateDistance = (x1, y1, x2, y2) =>
-  Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+function calculateDistance(x1, y1, x2, y2) {
+  const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  return distance;
+}
 
-const error = (err) => {
+function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
-};
+}
 
-const success = async (pos) => {
+async function success(pos) {
   try {
     const crd = pos.coords;
-    const restaurants = await fetchData(apiUrl + "/restaurants");
+    const restaurants = await fetchData(apiUrl + '/restaurants');
     console.log(restaurants);
-    restaurants.sort((a, b) => {
+    restaurants.sort(function (a, b) {
       const x1 = crd.latitude;
       const y1 = crd.longitude;
       const x2a = a.location.coordinates[1];
@@ -45,30 +47,30 @@ const success = async (pos) => {
     });
 
     for (const restaurant of restaurants) {
-      const tr = document.createElement("tr");
-      const name = document.createElement("td");
+      const tr = document.createElement('tr');
+      const name = document.createElement('td');
       name.innerText = restaurant.name;
-      const address = document.createElement("td");
+      const address = document.createElement('td');
       address.innerText = restaurant.address;
       tr.appendChild(name);
       tr.appendChild(address);
-      document.querySelector("table").appendChild(tr);
-      tr.addEventListener("click", async () => {
+      document.querySelector('table').appendChild(tr);
+      tr.addEventListener('click', async function () {
         try {
           // remove all highlights
-          const allHighs = document.querySelectorAll(".highlight");
+          const allHighs = document.querySelectorAll('.highlight');
           for (const high of allHighs) {
-            high.classList.remove("highlight");
+            high.classList.remove('highlight');
           }
           // add highlight
-          tr.classList.add("highlight");
+          tr.classList.add('highlight');
           // add restaurant data to modal
-          modal.innerHTML = "";
+          modal.innerHTML = '';
           const html = `<h3>${restaurant.name}</h3>
       <p>${restaurant.company}</p>
       <p>${restaurant.address} ${restaurant.postalCode} ${restaurant.city}</p>
       <p>${restaurant.phone}</p>`;
-          modal.insertAdjacentHTML("beforeend", html);
+          modal.insertAdjacentHTML('beforeend', html);
           // fetch menu
           const menu = await fetchData(
             apiUrl + `/restaurants/daily/${restaurant._id}/fi`
@@ -86,13 +88,13 @@ const success = async (pos) => {
             menuHtml += `
         <tr>
           <td>${course.name}</td>
-          <td>${course.diets ?? " - "}</td>
-          <td>${course.price ?? " - "}</td>
+          <td>${course.diets || ' - '}</td>
+          <td>${course.price || ' - '}</td>
         </tr>
         `;
           }
-          menuHtml += "</table>";
-          modal.insertAdjacentHTML("beforeend", menuHtml);
+          menuHtml += '</table>';
+          modal.insertAdjacentHTML('beforeend', menuHtml);
           modal.showModal();
         } catch (error) {
           alert(error.message);
@@ -102,6 +104,6 @@ const success = async (pos) => {
   } catch (error) {
     alert(error.message);
   }
-};
+}
 
 navigator.geolocation.getCurrentPosition(success, error, positionOptions);
