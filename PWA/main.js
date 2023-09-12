af4c6ea26ed3ee1cc8db9159bb3ed3e3f10df564
Elina <sankilaelina@gmail.com>
2023-09-12 19:16:32 +0300

    pwa done

--- PWA/main.js
+++ PWA/main.js
@@ -1,7 +1,7 @@
 (async () => {
   if ("serviceWorker" in navigator) {
     try {
-      const worker = await navigator.serviceWorker.register("./sw.js");
+      const worker = await navigator.serviceWorker.register("sw.js");
       console.log("Service Worker Registered");
     } catch (e) {
       console.log(e.message);
